d31cb30d4746cf0ac4ce344296963804fce2ea20
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-01-05 13:48:01 +0600

    header section done 80%

---
+++ src/Root/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Component/Navbar';
+import Footer from '../Component/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
